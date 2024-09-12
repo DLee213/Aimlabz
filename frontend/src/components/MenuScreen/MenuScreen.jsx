58a6e10ce860086dd273fbc19595e30f6181edf6
Matthew T. Iwane <mattiwane@gmail.com>
2024-09-11 22:29:18 -0700

    added hover effect on start game button

--- frontend/src/components/MenuScreen/MenuScreen.jsx
+++ frontend/src/components/MenuScreen/MenuScreen.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import gif from "../../assets/dance.gif"
 import './module.menuscreen.css';
+import { useState } from "react";
 
 export const MenuScreen = ({ points, startGame }) => {
+  const [cursorCoord, setCursorCoord] = useState({left: 0, top: 0})
+
+  function handleMouseMove(e) {
+    setCursorCoord({left: e.clientX})
+  }
+
   return (
     <div className="menu-screen">
-        <h1 className="prev-point-name">Highscore: {points}</h1>
+        <h1 className="prev-point-name">High Score: {points}</h1>
         <hr/>
+
         <button 
           onClick={startGame}
-          className="start-game"
+          onMouseMove={handleMouseMove}
+          className="start-game-btn"
         >
+          <img 
+            src={gif} 
+            alt="dancing marshmallow guy"
+            id="dancing-marshmallow-gif"  
+            style={{left: `${cursorCoord.left}px`}}        
+          />
           Start Game!
         </button>
     </div>
