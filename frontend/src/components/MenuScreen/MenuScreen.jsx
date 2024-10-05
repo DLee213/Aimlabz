213325082254bf440669cdb11b266da8b6185ce7
DLee213 <danielelliott213@gmail.com>
2024-10-04 17:28:48 -0700

    Added Aimlabs svg
    Co-authored-by: Matthew Iwane <Matthew-Iwane@users.noreply.github.com>

--- frontend/src/components/MenuScreen/MenuScreen.jsx
+++ frontend/src/components/MenuScreen/MenuScreen.jsx
@@ -1,11 +1,13 @@
 import gif from "../../assets/dance.gif"
 import './module.menuscreen.css';
 import { useState } from "react";
-
+import { motion } from "framer-motion";
 // Game Imports
 
 export const MenuScreen = ({ points, startGame, userName, leaderboard }) => {
   const [cursorCoord, setCursorCoord] = useState({ left: 0, top: 0 })
+  const [isNormalOpen, setIsNormalOpen] = useState(false);
+  const [isDifferentOpen, setIsDifferentOpen] = useState(false);
 
   function handleMouseMove(e) {
     setCursorCoord({ left: e.clientX })
@@ -53,6 +55,25 @@ export const MenuScreen = ({ points, startGame, userName, leaderboard }) => {
       <div className="game-options">
           <h1>GAME OPTIONS</h1>
 
+          <motion.div
+            layout
+            data-isOpen={isNormalOpen}
+            initial={{borderRadius: 50}}
+            className="parent"
+            onClick={() => setIsNormalOpen(!isNormalOpen)}
+            >
+              <motion.div layout className="child"/>
+            </motion.div>
+
+            <motion.div
+            layout
+            data-isOpen={isDifferentOpen}
+            initial={{borderRadius: 50}}
+            className="parent"
+            onClick={() => setIsDifferentOpen(!isDifferentOpen)}
+            >
+              <motion.div layout className="child"/>
+            </motion.div>
       </div>
     </div>
   )
